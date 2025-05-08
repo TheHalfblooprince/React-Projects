d717ad2201f4baf1c0426c9733203072fa1099ee
Muhmmad Wasi Ul Haq <115735858+WasiUlHaq11F@users.noreply.github.com>
2025-05-08 23:01:20 +0500

    Delete Postgram  directory

--- Postgram /client/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js (or tailwind.config.ts)
-export default {
-  darkMode: 'class', // Required
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
-  plugins: [],
-};
