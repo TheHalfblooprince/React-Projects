082341dfb4c28cc0d51deed24abb1853f54cb8a6
Muhmmad Wasi Ul Haq <115735858+WasiUlHaq11F@users.noreply.github.com>
2025-05-08 23:05:30 +0500

    Add files via upload

---
+++ Postgram/server/src/routes/loginRoute.ts
@@ -0,0 +1,55 @@
+import { User } from "../entities/User"
+import jwt from "jsonwebtoken"
+import express from "express"
+import bcrypt from "bcrypt"
+import { getRepository } from "typeorm";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const loginRouter = express.Router();
+
+loginRouter.post('/',async (req,res): Promise<any> => {
+
+    const {email,password} = req.body
+
+   try{
+
+        const userRepo = getRepository(User);
+        const user = await userRepo.findOne({
+            where: {email}
+        });
+        if(!user){
+            return res.status(404).json({
+                "message": "User not Found"
+            })
+        }
+
+        // match passwords.
+
+        const isMatch = bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(404).json({
+                "message": "Error! User not found"
+            })
+        }
+
+
+        //setup jwts. 
+        const secret = process.env.JWT_SECRET as string
+        const token = jwt.sign({userid:user.user_id}, secret, {expiresIn:'1hr'})
+       
+    return res.status(200).json({token,user})
+
+
+   }catch(error){
+    console.error("Error: ", error)
+    return res.status(500).json({
+        "message ": "Login Failed, Internal Server Error!"
+    })
+   }
+
+})
+
+
+export {loginRouter,}
